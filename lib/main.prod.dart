cfcde9f799e88ec12994666169b9d38be98cbaa1
I Gede Adi Martha Ardiana Putra <billytheburninginferno@gmail.com>
2022-02-17 01:26:28 +0800

    Docker build for production

---
+++ lib/main.prod.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:my_wealth/router.dart';
+import 'package:my_wealth/storage/local_box.dart';
+
+void main() {
+  // this is needed to ensure that all the binding already initialized before
+  // we plan to load the shared preferences.
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // initialize flutter application
+  Future.wait([
+    dotenv.load(fileName: "env/.prod.env"), // load the environment files
+    Hive.initFlutter(), // initialize Hive
+    LocalBox.init(), // initialize box (normal and secure)
+  ]).then((_) {
+    // if all the future success means application is initialized
+    debugPrint("💯 Application Initialized");
+  }).onError((error, stackTrace) {
+    // if caught error print all the error and the stack trace
+    debugPrint(error.toString());
+    debugPrint(stackTrace.toString());
+  }).whenComplete(() {
+    // run the application whatever happen with the future
+    runApp(const MyApp());
+  });
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    // return router page, as we will perform all the routing from router page
+    // to minimize the main page.
+    return const RouterPage();
+  }
+}

a0a41123a0d0167f5c9000f5a7648ba68cfada5b
I Gede Adi Martha Ardiana Putra <billytheburninginferno@gmail.com>
2022-07-24 16:16:48 +0800

    Automatic tag docker based on the env file

--- docker_build.sh
+++ docker_build.sh
@@ -11,3 +11,13 @@ flutter build web --release -t lib/main.prod.dart
 
 # build the docker based on the build
 docker build -t adimartha/my_wealth .
+
+# once finished build then get the current tag from the environment file
+tag = `cat env/.prod.env | sed '2q;d' | awk -F "=" '{print $2}' | sed "s/['\"]//g" | awk -F "-" '{print $1}'`
+
+# then tag the latest docker image to the current tag
+docker image tag adimartha/my_wealth:latest adimartha/my_wealth:$tag
+
+# push both of the image to the docker repo
+docker image push adimartha/my_wealth:latest
+docker image push adimartha/my_wealth:$tag

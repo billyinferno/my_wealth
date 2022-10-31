4374846ad13510a4b4a3c545a5ee5da438c2dc0e
I Gede Adi Martha <billytheburninginferno@gmail.com>
2022-11-01 00:34:04 +0800

    Change binary computation from string manipulation to using binary operator

--- lib/utils/function/binary_computation.dart
+++ lib/utils/function/binary_computation.dart
@@ -1,36 +1,66 @@
+import 'dart:math';
+
 class Bit {
-  int _value = 0;
-  List<String> _binArr = [];
+  int? _value;
 
   void set(int value) {
     _value = value;
-
-    String binStr = _value.toRadixString(2).padLeft(16, '0');
-    _binArr = List<String>.generate(binStr.length, (index) {
-      return binStr[index];
-    });
   }
 
   bool operator [](int index) {
+    if (_value == null) {
+      throw Exception("Value not being set yet");
+    }
+
     if (index < 0 || index > 15) {
       throw Exception("Bit position should be between 0-15");
     }
 
-    return (int.tryParse(_binArr[index])! == 0 ? false : true);
+    // to check the bit we can just perform right shift index
+    int tmp = _value! >> index;
+
+    // check if tmp is odd, if odd, it means that the bit is enabled
+    return (tmp % 2 == 0 ? false : true);
   }
 
   operator []=(int index, int value) {
+    if (_value == null) {
+      throw Exception("Value not being set yet");
+    }
+
     if (index < 0 || index > 15) {
       throw Exception("Bit position should be between 0-15");
     }
 
-    _binArr[index] = value.toString();
+    if (value != 0 && value != 1) {
+      throw Exception("Bit value should be 0 or 1");
+    }
+
+    // check whether we want to set the value or else
+    if (value == 1) {
+      // this is easy, we can just do operator |
+      int powVal = pow(2, index) as int;
+      _value = _value! | powVal;
+    }
+    else {
+      // this is setting up the bit to 0, so now check if this bit is being
+      // enabled or not first
+      int tmp = _value! >> index;
+
+      // if odd, then it means the bit is being setup
+      if (tmp % 2 == 1) {
+        // subtract the _value with the powVal
+        int powVal = pow(2, index) as int;
+        _value = _value! - powVal;
+      }
+    }
   }
 
   int toInt() {
-    String binStr = _binArr.join();
-    
-    // convert from binary to int
-    return int.parse(binStr, radix: 2);
+    if (_value == null) {
+      throw Exception("Value not being set yet");
+    }
+
+    return _value!;
   }
 }

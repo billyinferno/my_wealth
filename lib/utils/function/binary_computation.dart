ca757f207205e65f22fb74dff7941f41406312b9
I Gede Adi Martha <billytheburninginferno@gmail.com>
2022-11-15 16:56:13 +0800

    Fix assert checking for the bit and value

--- lib/utils/function/binary_computation.dart
+++ lib/utils/function/binary_computation.dart
@@ -10,15 +10,15 @@ class Bit {
   }
 
   bool operator [](int index) {
-    assert(index < 0 || index > 15, "Bit position should be between 0-15");
+    assert(index >= 0 && index <= 15, "Bit position should be between 0-15");
 
     // check if the value is odd after shr, if odd, it means that the bit is enabled
     return ((_value >> index) % 2 == 0 ? false : true);
   }
 
   operator []=(int index, int value) {
-    assert(index < 0 || index > 15, "Bit position should be between 0-15");
-    assert(value != 0 && value != 1, "Bit value should be 0 or 1");
+    assert(index >= 0 && index <= 15, "Bit position should be between 0-15");
+    assert(value == 0 || value == 1, "Bit value should be 0 or 1");
 
     // check whether we want to set the value or else
     if (value == 1) {

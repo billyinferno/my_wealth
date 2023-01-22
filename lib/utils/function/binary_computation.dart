7f32ad2d5bebdc7362d6435611c3e6ca18496e96
I Gede Adi Martha <billytheburninginferno@gmail.com>
2023-01-22 18:07:31 +0800

    Change the binary computation to using string instead of numeric manipulation as it got error result and lazy to actually debug why it happen

--- lib/utils/function/binary_computation.dart
+++ lib/utils/function/binary_computation.dart
@@ -1,37 +1,47 @@
 class Bit {
   int _value = 0;
+  List<String> _binArr = [];
 
   // create a constructor where we can assign a value if needed
   // and if not assigned, it will be defaulted  to 0.
-  Bit({int? value}): _value = (value ?? 0);
+  Bit({int? value}) {
+    if (value != null) {
+      set(value);
+    }
+    else {
+      // defauled to zero
+      set(0);
+    }
+  }
 
   void set(int value) {
     _value = value;
+
+    String binStr = _value.toRadixString(2).padLeft(16, '0');
+    _binArr = List<String>.generate(binStr.length, (index) {
+      return binStr[index];
+    });
   }
 
   bool operator [](int index) {
     assert(index >= 0 && index <= 15, "Bit position should be between 0-15");
 
     // check if the value is odd after shr, if odd, it means that the bit is enabled
-    return ((_value >> index) % 2 == 0 ? false : true);
+    return (int.tryParse(_binArr[index])! == 0 ? false : true);
   }
 
   operator []=(int index, int value) {
     assert(index >= 0 && index <= 15, "Bit position should be between 0-15");
     assert(value == 0 || value == 1, "Bit value should be 0 or 1");
 
-    // check whether we want to set the value or else
-    if (value == 1) {
-      // do the or of the shl 1 of idx
-      _value |= (1 << index);
-    }
-    else {
-      // this is setting up the bit to 0
-      _value &= ~(1 << index);
-    }
+    // set the binary array with the value passed
+    _binArr[index] = value.toString();
   }
 
   int toInt() {
-    return _value;
+    String binStr = _binArr.join();
+    
+    // convert from binary to int
+    return int.parse(binStr, radix: 2);
   }
 }

d0962c100003ddbc09dac218b9a688bb0baa9660
billyinferno <billytheburninginferno@gmail.com>
2023-07-14 00:34:02 +0800

    Add no tree shake icons and verbose when compile due error from flutter when perform tree shake

--- docker_build.sh
+++ docker_build.sh
@@ -7,7 +7,7 @@ flutter clean
 flutter pub get
 
 # rebuild the flutter web apps
-flutter build web --release -t lib/main.prod.dart
+flutter build web --release -t lib/main.prod.dart --verbose --no-tree-shake-icons
 
 # build the docker based on the build
 docker build -t adimartha/my_wealth .

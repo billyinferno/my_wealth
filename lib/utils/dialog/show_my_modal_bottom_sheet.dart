be9248337f30e571e3ea802011ce2662e94ae037
I Gede Adi Martha <billytheburninginferno@gmail.com>
2024-01-01 12:54:39 +0800

    Separate the modal bottom sheet so we can re-use it for other page/widget to align the overall design on the application.

---
+++ lib/utils/dialog/show_my_modal_bottom_sheet.dart
@@ -0,0 +1,130 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:my_wealth/themes/colors.dart';
+
+class ShowMyModalBottomSheet {
+  final BuildContext context;
+  final String? title;
+  final Color? bgColor;
+  final double? maxHeight;
+  final Map<String, String> filterList;
+  final String filterMode;
+  final Function(String) onFilterSelect;
+
+  ShowMyModalBottomSheet({
+    required this.context,
+    this.title,
+    this.bgColor,
+    this.maxHeight,
+    required this.filterList,
+    required this.filterMode,
+    required this.onFilterSelect}
+  );
+
+  Future<void> show() {
+    return showModalBottomSheet<void>(
+      context: context,
+      backgroundColor: Colors.transparent,
+      isDismissible: true,
+      builder: (context) {
+        return Container(
+          height: (min((filterList.length * 40) + 50, (maxHeight ?? 400))),
+          margin: const EdgeInsets.fromLTRB(10, 10, 10, 25),
+          padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+          decoration: BoxDecoration(
+            color: (bgColor ?? Colors.black),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Center(
+                child: Text((title ?? "Select Filter")),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: filterList.length,
+                  itemBuilder: ((context, index) {
+                    String key =
+                        filterList.keys.elementAt(index);
+              
+                    return GestureDetector(
+                      onTap: (() {
+                        onFilterSelect(key);
+                        // remove the modal sheet
+                        Navigator.pop(context);
+                      }),
+                      child: Container(
+                        width: double.infinity,
+                        padding: const EdgeInsets.all(10),
+                        decoration: const BoxDecoration(
+                            border: Border(
+                                bottom: BorderSide(
+                          color: primaryLight,
+                          width: 1.0,
+                          style: BorderStyle.solid,
+                        ))),
+                        child: Row(
+                          crossAxisAlignment:
+                              CrossAxisAlignment.center,
+                          mainAxisAlignment:
+                              MainAxisAlignment.start,
+                          children: <Widget>[
+                            Container(
+                              width: 20,
+                              height: 20,
+                              decoration: BoxDecoration(
+                                  color: (filterMode == key
+                                      ? accentDark
+                                      : Colors.transparent),
+                                  borderRadius:
+                                      BorderRadius.circular(2),
+                                  border: Border.all(
+                                    color: accentDark,
+                                    width: 1.0,
+                                    style: BorderStyle.solid,
+                                  )),
+                              child: Center(
+                                child: Text(
+                                  key,
+                                  style: TextStyle(
+                                    fontSize: 10,
+                                    color:
+                                        (filterMode == key
+                                            ? textPrimary
+                                            : accentColor),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              filterList[key]!,
+                              style: TextStyle(
+                                color: (filterMode == key
+                                    ? accentColor
+                                    : textPrimary),
+                                fontWeight:
+                                    (filterMode == key
+                                        ? FontWeight.bold
+                                        : FontWeight.normal),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  })
+                ),
+              ),
+            ],
+          )
+        );
+      },
+    );
+  }
+}
--- lib/widgets/components/search_box.dart
+++ lib/widgets/components/search_box.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ionicons/ionicons.dart';
 import 'package:my_wealth/themes/colors.dart';
+import 'package:my_wealth/utils/dialog/show_my_modal_bottom_sheet.dart';
 
 class SearchBox extends StatefulWidget {
   final String filterMode;
@@ -8,24 +9,39 @@ class SearchBox extends StatefulWidget {
   final Function(String) onFilterSelect;
   final String filterSort;
   final Function(String) onSortSelect;
-  const SearchBox({Key? key, required this.filterMode, required this.filterList, required this.filterSort, required this.onFilterSelect, required this.onSortSelect}) : super(key: key);
+  final Color? bgColor;
+  final Color? fgColor;
+  const SearchBox({
+    Key? key,
+    required this.filterMode,
+    required this.filterList,
+    required this.filterSort,
+    required this.onFilterSelect,
+    required this.onSortSelect,
+    this.bgColor,
+    this.fgColor,
+  }) : super(key: key);
 
   @override
   State<SearchBox> createState() => _SearchBoxState();
 }
 
 class _SearchBoxState extends State<SearchBox> {
-  final TextStyle _filterTypeSelected = const TextStyle(fontSize: 10, color: accentColor, fontWeight: FontWeight.bold);
-  final TextStyle _filterTypeUnselected = const TextStyle(fontSize: 10, color: primaryLight, fontWeight: FontWeight.normal);
-  
-  // late String _filterMode;
-  // late Map<String, String> _filterList;
-  // late String _filterSort;
+  late TextStyle _filterTypeSelected;
+  late TextStyle _filterTypeUnselected;
 
   @override
   void initState() {
     // move all the data from parent to this widget, as we will perform set state
     // here that will not affect the parent widget.
+    _filterTypeSelected = TextStyle(
+        fontSize: 10,
+        color: (widget.fgColor ?? accentColor),
+        fontWeight: FontWeight.bold);
+    
+    _filterTypeUnselected = const TextStyle(
+      fontSize: 10, color: primaryLight, fontWeight: FontWeight.normal);
+    
     super.initState();
   }
 
@@ -33,7 +49,7 @@ class _SearchBoxState extends State<SearchBox> {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
-      color: primaryDark,
+      color: (widget.bgColor ?? primaryDark),
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.start,
@@ -45,91 +61,20 @@ class _SearchBoxState extends State<SearchBox> {
               fontSize: 10,
             ),
           ),
-          const SizedBox(width: 5,),
+          const SizedBox(
+            width: 5,
+          ),
           Expanded(
             child: GestureDetector(
               onTap: (() {
-                showModalBottomSheet<void>(
+                ShowMyModalBottomSheet(
                   context: context,
-                  backgroundColor: Colors.transparent,
-                  isDismissible: true,
-                  builder:(context) {
-                    return Container(
-                      height: ((widget.filterList.length * 40) + 50),
-                      margin: const EdgeInsets.fromLTRB(10, 10, 10, 25),
-                      padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                      decoration: BoxDecoration(
-                        color: Colors.black,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: <Widget>[
-                          const Center(
-                            child: Text("Select Filter"),
-                          ),
-                          ...widget.filterList.entries.map((e) => GestureDetector(
-                            onTap: (() {
-                              widget.onFilterSelect(e.key);
-                              // remove the modal sheet
-                              Navigator.pop(context);
-                            }),
-                            child: Container(
-                              width: double.infinity,
-                              padding: const EdgeInsets.all(10),
-                              decoration: const BoxDecoration(
-                                border: Border(
-                                  bottom: BorderSide(
-                                    color: primaryLight,
-                                    width: 1.0,
-                                    style: BorderStyle.solid,
-                                  )
-                                )
-                              ),
-                              child: Row(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  Container(
-                                    width: 20,
-                                    height: 20,
-                                    decoration: BoxDecoration(
-                                      color: (widget.filterMode == e.key ? accentDark : Colors.transparent),
-                                      borderRadius: BorderRadius.circular(2),
-                                      border: Border.all(
-                                        color: accentDark,
-                                        width: 1.0,
-                                        style: BorderStyle.solid,
-                                      )
-                                    ),
-                                    child: Center(
-                                      child: Text(
-                                        e.key,
-                                        style: TextStyle(
-                                          fontSize: 10,
-                                          color: (widget.filterMode == e.key ? textPrimary : accentColor),
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                  const SizedBox(width: 10,),
-                                  Text(
-                                    e.value,
-                                    style: TextStyle(
-                                      color: (widget.filterMode == e.key ? accentColor : textPrimary),
-                                      fontWeight: (widget.filterMode == e.key ? FontWeight.bold : FontWeight.normal),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          )),
-                        ],
-                      )
-                    );
-                  },
-                );
+                  filterList: widget.filterList,
+                  filterMode: widget.filterMode,
+                  onFilterSelect: ((value) {
+                    widget.onFilterSelect(value);
+                  })
+                ).show();
               }),
               child: Container(
                 padding: const EdgeInsets.all(5),
@@ -146,9 +91,12 @@ class _SearchBoxState extends State<SearchBox> {
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: <Widget>[
                     Expanded(
-                      child: Text(widget.filterList[widget.filterMode] ?? 'Code'),
+                      child:
+                          Text(widget.filterList[widget.filterMode] ?? 'Code'),
                     ),
-                    const SizedBox(width: 5,),
+                    const SizedBox(
+                      width: 5,
+                    ),
                     const Icon(
                       Ionicons.caret_down,
                       color: accentColor,
@@ -159,7 +107,9 @@ class _SearchBoxState extends State<SearchBox> {
               ),
             ),
           ),
-          const SizedBox(width: 5,),
+          const SizedBox(
+            width: 5,
+          ),
           GestureDetector(
             onTap: (() {
               if (widget.filterSort != "ASC") {
@@ -171,12 +121,16 @@ class _SearchBoxState extends State<SearchBox> {
               child: Center(
                 child: Text(
                   "ASC",
-                  style: (widget.filterSort == "ASC" ? _filterTypeSelected : _filterTypeUnselected),
+                  style: (widget.filterSort == "ASC"
+                      ? _filterTypeSelected
+                      : _filterTypeUnselected),
                 ),
               ),
             ),
           ),
-          const SizedBox(width: 2,),
+          const SizedBox(
+            width: 2,
+          ),
           GestureDetector(
             onTap: (() {
               if (widget.filterSort != "DESC") {
@@ -188,7 +142,9 @@ class _SearchBoxState extends State<SearchBox> {
               child: Center(
                 child: Text(
                   "DESC",
-                  style: (widget.filterSort == "DESC" ? _filterTypeSelected : _filterTypeUnselected),
+                  style: (widget.filterSort == "DESC"
+                      ? _filterTypeSelected
+                      : _filterTypeUnselected),
                 ),
               ),
             ),

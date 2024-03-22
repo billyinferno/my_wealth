193dfb705c79e321f1e8b239b70ed8d2615b1968
billyinferno <billytheburninginferno@gmail.com>
2024-03-22 20:37:21 +0800

    Add  additional Parameter for POST Add whether JWT is required or not as optional parameter when sending POST request to the API server.

--- lib/utils/net/netutils.dart
+++ lib/utils/net/netutils.dart
@@ -42,14 +42,24 @@ class NetException {
 class NetUtils {
   static String? bearerToken;
 
+  /// refreshJWT
+  /// Refresh current JWT token with the one stored on the Encrypted Box under
+  /// User Shared Preferences.
   static void refreshJWT() {
     bearerToken = UserSharedPreferences.getUserJWT();
   }
 
+  /// clearJWT
+  /// Clear current JWT Token and set it as NULL
   static void clearJWT() {
     bearerToken = null;
   }
 
+  /// get
+  /// This is to sending GET request to the API Server
+  /// Parameter needed for this are:
+  /// - required : url         : String
+  /// - optional : params      : Map<String, dynamic>
   static Future get({required String url, Map<String, dynamic>? params}) async {
     // check if bearer token is null? if null then get from UserSharedPreferences
     bearerToken ??= UserSharedPreferences.getUserJWT();
@@ -101,17 +111,45 @@ class NetUtils {
     );
   }
 
-  static Future post({required String url, Map<String, dynamic>? params, required Map<String, dynamic> body}) async {
-    // check if bearer token is null? if null then get from UserSharedPreferences
-    bearerToken ??= UserSharedPreferences.getUserJWT();
+  /// post
+  /// This is to sending POST request to the API Server
+  /// Parameter needed for this are:
+  /// - required : url         : String
+  /// - optional : params      : Map<String, dynamic>
+  /// - required : body        : Map<String, dynamic>
+  /// - optional : requiredJWT : bool
+  static Future post({required String url, Map<String, dynamic>? params, required Map<String, dynamic> body, bool? requiredJWT}) async {
+    // check the JWT requirement
+    bool isRequiredJWT = (requiredJWT ?? true);
+    
+    // default the headers value as empty
+    Map<String, String> headers = {};
 
-    // check to ensure it's not empty
-    if (bearerToken == null) {
-      throw const NetException(
-        code: 403,
-        type: NetType.initialize,
-        message: "Bearer token empty"
-      );
+    // check if we need JWT on the request or not?
+    if (isRequiredJWT) {
+      // check if bearer token is null? if null then get from UserSharedPreferences
+      bearerToken ??= UserSharedPreferences.getUserJWT();
+
+      // check to ensure it's not empty
+      if (bearerToken == null) {
+        throw const NetException(
+          code: 403,
+          type: NetType.initialize,
+          message: "Bearer token empty"
+        );
+      }
+
+      // set the headers with bearer token
+      headers = {
+        HttpHeaders.authorizationHeader: "Bearer $bearerToken",
+        'Content-Type': 'application/json',
+      };
+    }
+    else {
+      // set the headers with content type only
+      headers = {
+        'Content-Type': 'application/json',
+      };
     }
 
     // generate the additional params
@@ -123,10 +161,7 @@ class NetUtils {
     // bearer token is not empty, we can perform call to the API
     final response = await http.post(
       uri,
-      headers: {
-        HttpHeaders.authorizationHeader: "Bearer $bearerToken",
-        'Content-Type': 'application/json',
-      },
+      headers: headers,
       body: jsonEncode(body)
     ).timeout(
       Duration(seconds: Globals.apiTimeOut),
@@ -153,6 +188,11 @@ class NetUtils {
     );
   }
 
+  /// delete
+  /// This is to sending DELETE request to the API Server
+  /// Parameter needed for this are:
+  /// - required : url         : String
+  /// - optional : params      : Map<String, dynamic>
   static Future delete({required String url, Map<String, dynamic>? params}) async {
     // check if bearer token is null? if null then get from UserSharedPreferences
     bearerToken ??= UserSharedPreferences.getUserJWT();
@@ -204,6 +244,12 @@ class NetUtils {
     );
   }
 
+  /// patch
+  /// This is to sending PATCH request to the API Server
+  /// Parameter needed for this are:
+  /// - required : url         : String
+  /// - optional : params      : Map<String, dynamic>
+  /// - required : body        : Map<String, dynamic>
   static Future patch({required String url, Map<String, dynamic>? params, required Map<String, dynamic> body}) async {
     // check if bearer token is null? if null then get from UserSharedPreferences
     bearerToken ??= UserSharedPreferences.getUserJWT();
@@ -256,6 +302,12 @@ class NetUtils {
     );
   }
 
+  /// put
+  /// This is to sending PUT request to the API Server
+  /// Parameter needed for this are:
+  /// - required : url         : String
+  /// - optional : params      : Map<String, dynamic>
+  /// - required : body        : Map<String, dynamic>
   static Future put({required String url, Map<String, dynamic>? params, required Map<String, dynamic> body}) async {
     // check if bearer token is null? if null then get from UserSharedPreferences
     bearerToken ??= UserSharedPreferences.getUserJWT();

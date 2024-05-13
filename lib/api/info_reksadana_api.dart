27b9b5081a5cb75a3bd71230f5eec0b51ba14ec7
I Gede Adi Martha <billytheburninginferno@gmail.com>
2024-05-13 09:47:29 +0800

    Add the API to get mutual fund price using from and to date This will ensure the flexibility from front end to get the number of data that we want to present on the front end.

--- lib/api/info_reksadana_api.dart
+++ lib/api/info_reksadana_api.dart
@@ -25,4 +25,27 @@ class InfoReksadanaAPI {
     }
     return listInfoReksadana;
   }
+
+  Future<List<InfoReksadanaModel>> getInfoReksadanaDate(
+    int companyId,
+    DateTime from,
+    DateTime to
+  ) async {
+    // get reksadana information using netutils
+    final String body = await NetUtils.get(
+      url: '${Globals.apiInfoReksadana}/id/$companyId/from/${Globals.dfyyyyMMdd.format(from)}/to/${Globals.dfyyyyMMdd.format(to)}'
+    ).onError((error, stackTrace) {
+        throw Exception(error);
+      }
+    );
+
+    // parse the response to reksdana information
+    CommonArrayModel commonModel = CommonArrayModel.fromJson(jsonDecode(body));
+    List<InfoReksadanaModel> listInfoReksadana = [];
+    for (var data in commonModel.data) {
+      InfoReksadanaModel index = InfoReksadanaModel.fromJson(data['attributes']);
+      listInfoReksadana.add(index);
+    }
+    return listInfoReksadana;
+  }
 }

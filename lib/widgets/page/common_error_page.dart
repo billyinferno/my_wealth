c88176d37ebaf74c656ea8e78ec090e712c7da06
billyinferno <billytheburninginferno@gmail.com>
2024-07-16 23:48:54 +0800

    Add new isNeedScaffold on the common error page. This is to differentiate between common error page that need to showed scaffold and appbar, or just the container with error text as some of the page will already inside scaffold, so no need to add another scaffold when we want to showed the common error page.

--- lib/widgets/page/common_error_page.dart
+++ lib/widgets/page/common_error_page.dart
@@ -4,28 +4,56 @@ import 'package:my_wealth/themes/colors.dart';
 
 class CommonErrorPage extends StatelessWidget {
   final String errorText;
-  const CommonErrorPage({super.key, required this.errorText});
+  final bool? isNeedScaffold;
+  const CommonErrorPage({super.key, required this.errorText, this.isNeedScaffold});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Center(
-          child: Text(
-            "Error",
-            style: TextStyle(
-              color: secondaryColor,
+    if (isNeedScaffold ?? true) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Center(
+            child: Text(
+              "Error",
+              style: TextStyle(
+                color: secondaryColor,
+              ),
             ),
           ),
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: (() async {
+              Navigator.pop(context);
+            }),
+          ),
         ),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: (() async {
-            Navigator.pop(context);
-          }),
-        ),
-      ),
-      body: Container(
+        body: Container(
+          width: double.infinity,
+          color: primaryColor,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              const Icon(
+                Ionicons.alert_circle,
+                color: secondaryColor,
+                size: 20,
+              ),
+              const SizedBox(height: 10,),
+              Text(
+                errorText,
+                style: const TextStyle(
+                  color: secondaryColor,
+                  fontSize: 15,
+                ),
+              ),
+            ],
+          ),
+        )
+      );
+    }
+    else {
+      return Container(
         width: double.infinity,
         color: primaryColor,
         child: Column(
@@ -47,7 +75,7 @@ class CommonErrorPage extends StatelessWidget {
             ),
           ],
         ),
-      )
-    );
+      );
+    }
   }
 }

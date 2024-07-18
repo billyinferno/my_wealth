b7dd08752dd94c054e013063fe145d793df2ef6e
billyinferno <billytheburninginferno@gmail.com>
2024-07-18 11:42:41 +0800

    Add isAfterDate and isSameOrAfter function to check date

--- lib/utils/function/date_utils.dart
+++ lib/utils/function/date_utils.dart
@@ -61,8 +61,25 @@ bool isBeforeDate({required DateTime date1, required DateTime date2}) {
   return false;
 }
 
+bool isAfterDate({required DateTime date1, required DateTime date2}) {
+  final DateTime dt1 = DateTime(date1.year, date1.month, date1.day);
+  final DateTime dt2 = DateTime(date2.year, date2.month, date2.day);
+
+  if (dt1.isAfter(dt2)) {
+    return true;
+  }
+  return false;
+}
+
 bool isSameOrBefore({required DateTime date, required DateTime checkDate}) {
   if (isSameDate(date1: date, date2: checkDate) || isBeforeDate(date1: date, date2: checkDate)) {
+    return true;
+  }
+  return false;
+}
+
+bool isSameOrAfter({required DateTime date, required DateTime checkDate}) {
+  if (isSameDate(date1: date, date2: checkDate) || isAfterDate(date1: date, date2: checkDate)) {
     return true;
   }
   return false;

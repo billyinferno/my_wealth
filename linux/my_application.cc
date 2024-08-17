746d1dedac131160eacc3f15e29a6c77586242e8
billyinferno <billytheburninginferno@gmail.com>
2024-08-17 10:42:31 +0800

    Change application information and default window size both on windows and linux

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -40,14 +40,14 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "my_wealth");
+    gtk_header_bar_set_title(header_bar, "myWealth");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "my_wealth");
+    gtk_window_set_title(window, "myWealth");
   }
 
-  gtk_window_set_default_size(window, 1280, 720);
+  gtk_window_set_default_size(window, 428, 926);
   gtk_widget_show(GTK_WIDGET(window));
 
   g_autoptr(FlDartProject) project = fl_dart_project_new();

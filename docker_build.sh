756945ff96fc716c58c3dce18aefd14e7c2ad77d
billyinferno <billytheburninginferno@gmail.com>
2024-08-23 00:20:00 +0800

    Change back docker build to WASM as we already found out the issue on the mime type

--- docker_build.sh
+++ docker_build.sh
@@ -8,7 +8,7 @@ flutter pub get
 
 # rebuild the flutter web apps
 # flutter build web --release -t lib/main.prod.dart --verbose --no-tree-shake-icons
-flutter build web --release -t lib/main.prod.dart --verbose
+flutter build web --release --wasm -t lib/main.prod.dart --verbose
 
 # build the docker based on the build
 docker build -t adimartha/my_wealth .

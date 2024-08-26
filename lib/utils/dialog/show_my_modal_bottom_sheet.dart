2b8c328020c5630f1d54f01432de783f147dbf7c
billyinferno <billytheburninginferno@gmail.com>
2024-08-26 12:42:21 +0800

    Change modal bottom sheet for filter to use safe area

--- lib/utils/dialog/show_my_modal_bottom_sheet.dart
+++ lib/utils/dialog/show_my_modal_bottom_sheet.dart
@@ -30,7 +30,7 @@ class ShowMyModalBottomSheet {
       builder: (context) {
         return Container(
           height: (min((filterList.length * 40) + 50, (maxHeight ?? 400))),
-          margin: const EdgeInsets.fromLTRB(10, 10, 10, 25),
+          margin: const EdgeInsets.fromLTRB(10, 10, 10, 10),
           padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
           decoration: BoxDecoration(
             color: (bgColor ?? Colors.black),
@@ -44,81 +44,87 @@ class ShowMyModalBottomSheet {
                 child: Text((title ?? "Select Filter")),
               ),
               Expanded(
-                child: ListView.builder(
-                  itemCount: filterList.length,
-                  itemBuilder: ((context, index) {
-                    String key =
-                        filterList.keys.elementAt(index);
-              
-                    return GestureDetector(
-                      onTap: (() {
-                        onFilterSelect(key);
-                        // remove the modal sheet
-                        Navigator.pop(context);
-                      }),
-                      child: Container(
-                        width: double.infinity,
-                        padding: const EdgeInsets.all(10),
-                        decoration: const BoxDecoration(
-                            border: Border(
-                                bottom: BorderSide(
-                          color: primaryLight,
-                          width: 1.0,
-                          style: BorderStyle.solid,
-                        ))),
-                        child: Row(
-                          crossAxisAlignment:
-                              CrossAxisAlignment.center,
-                          mainAxisAlignment:
-                              MainAxisAlignment.start,
-                          children: <Widget>[
-                            Container(
-                              width: 20,
-                              height: 20,
-                              decoration: BoxDecoration(
-                                  color: (filterMode == key
-                                      ? accentDark
-                                      : Colors.transparent),
-                                  borderRadius:
-                                      BorderRadius.circular(2),
+                child: MySafeArea(
+                  bottomPadding: 15, // since we already add 10 on the default
+                  child: ListView.builder(
+                    itemCount: filterList.length,
+                    itemBuilder: ((context, index) {
+                      String key = filterList.keys.elementAt(index);
+                                
+                      return GestureDetector(
+                        onTap: (() {
+                          onFilterSelect(key);
+                          // remove the modal sheet
+                          Navigator.pop(context);
+                        }),
+                        child: Container(
+                          width: double.infinity,
+                          padding: const EdgeInsets.all(10),
+                          decoration: const BoxDecoration(
+                              border: Border(
+                                  bottom: BorderSide(
+                            color: primaryLight,
+                            width: 1.0,
+                            style: BorderStyle.solid,
+                          ))),
+                          child: Row(
+                            crossAxisAlignment:
+                                CrossAxisAlignment.center,
+                            mainAxisAlignment:
+                                MainAxisAlignment.start,
+                            children: <Widget>[
+                              Container(
+                                width: 20,
+                                height: 20,
+                                decoration: BoxDecoration(
+                                  color: (
+                                    filterMode == key ?
+                                    accentDark :
+                                    Colors.transparent
+                                  ),
+                                  borderRadius: BorderRadius.circular(2),
                                   border: Border.all(
                                     color: accentDark,
                                     width: 1.0,
                                     style: BorderStyle.solid,
-                                  )),
-                              child: Center(
-                                child: Text(
-                                  key,
-                                  style: TextStyle(
-                                    fontSize: 10,
-                                    color:
-                                        (filterMode == key
-                                            ? textPrimary
-                                            : accentColor),
+                                  )
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    key,
+                                    style: TextStyle(
+                                      fontSize: 10,
+                                      color: (
+                                        filterMode == key ?
+                                        textPrimary :
+                                        accentColor
+                                      ),
+                                    ),
                                   ),
                                 ),
                               ),
-                            ),
-                            const SizedBox(
-                              width: 10,
-                            ),
-                            Text(
-                              filterList[key]!,
-                              style: TextStyle(
-                                color: (filterMode == key
-                                    ? accentColor
-                                    : textPrimary),
-                                fontWeight:
-                                    (filterMode == key
-                                        ? FontWeight.bold
-                                        : FontWeight.normal),
+                              const SizedBox(width: 10,),
+                              Text(
+                                filterList[key]!,
+                                style: TextStyle(
+                                  color: (
+                                    filterMode == key ?
+                                    accentColor :
+                                    textPrimary
+                                  ),
+                                  fontWeight: (
+                                    filterMode == key ?
+                                    FontWeight.bold :
+                                    FontWeight.normal
+                                  ),
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
-                    );
-                  })
+                      );
+                    })
+                  ),
                 ),
               ),
             ],

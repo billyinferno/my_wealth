0cbdd96d528fecfdb189993266c3e46949685bb3
I Gede Adi Martha <billytheburninginferno@gmail.com>
2024-09-20 19:31:54 +0800

    Add bar decoration variable. This is to change if there are any decoration will be added on the bar, such as border, etc. This can be used to separate and easily distinguish the bar chart with the background.

--- lib/widgets/chart/bar_chart.dart
+++ lib/widgets/chart/bar_chart.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_wealth/_index.g.dart';
 
 class BarChartData {
   final String title;
@@ -16,19 +17,44 @@ class BarChartData {
 
 class BarChart extends StatelessWidget {
   final List<BarChartData> data;
-  final EdgeInsets? padding;
-  final BoxDecoration? decoration;
-  final double? barHeight;
-  final double? legendPadding;
-  final bool? showLegend;
+  final EdgeInsets padding;
+  final BoxDecoration decoration;
+  final double barHeight;
+  final BoxDecoration barDecoration;
+  final double legendPadding;
+  final bool showLegend;
   const BarChart({
     super.key,
     required this.data,
-    this.padding,
-    this.decoration,
-    this.barHeight,
-    this.legendPadding,
-    this.showLegend
+    this.padding = const EdgeInsets.fromLTRB(10, 0, 10, 0),
+    this.decoration = const BoxDecoration(),
+    this.barHeight = 25,
+    this.barDecoration = const BoxDecoration(
+      border: Border(
+        bottom: BorderSide(
+          color: primaryDark,
+          width: 1.0,
+          style: BorderStyle.solid,
+        ),
+        left: BorderSide(
+          color: primaryDark,
+          width: 1.0,
+          style: BorderStyle.solid,
+        ),
+        right: BorderSide(
+          color: primaryDark,
+          width: 1.0,
+          style: BorderStyle.solid,
+        ),
+        top: BorderSide(
+          color: primaryDark,
+          width: 1.0,
+          style: BorderStyle.solid,
+        ),
+      )
+    ),
+    this.legendPadding = 5,
+    this.showLegend = true,
   });
 
   @override
@@ -38,33 +64,30 @@ class BarChart extends StatelessWidget {
       return const SizedBox.shrink();
     }
 
-    EdgeInsets currentPadding = (padding ?? const EdgeInsets.all(0));
-    BoxDecoration currentDecoration = (decoration ?? const BoxDecoration());
-    double currentLegendPadding = (legendPadding ?? 5);
-    bool isShowLegend = (showLegend ?? true);
-
     return Container(
-      padding: currentPadding,
-      decoration: currentDecoration,
+      padding: padding,
+      decoration: decoration,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
+          const SizedBox(height: 10,),
           Container(
             width: double.infinity,
-            padding: const EdgeInsets.all(10),
+            decoration: barDecoration,
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.start,
               children: _generateBar(),
             ),
           ),
+          const SizedBox(height: 5,),
           Visibility(
-            visible: isShowLegend,
-            child: SizedBox(height: currentLegendPadding,)
+            visible: showLegend,
+            child: SizedBox(height: legendPadding,)
           ),
           Visibility(
-            visible: isShowLegend,
+            visible: showLegend,
             child: Container(
               padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
               child: Row(
@@ -111,7 +134,6 @@ class BarChart extends StatelessWidget {
   List<Widget> _generateBar() {
     List<Widget> result = [];
     int flexCalc = 0;
-    double currentBarHeight = (barHeight ?? 25);
 
     // loop thru all the data
     for (BarChartData bar in data) {
@@ -127,7 +149,7 @@ class BarChart extends StatelessWidget {
           Expanded(
             flex: flexCalc,
             child: Container(
-              height: currentBarHeight,
+              height: barHeight,
               color: bar.color,
             ),
           )
